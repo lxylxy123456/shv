0a465b6721dfe31b14b7ffaeb31bf8a1b4520bfa
Eric Li <lixiaoyi13691419520@gmail.com>
2023-08-20 15:17:35 -0700

    Add copyright to all files

--- libc_stdio.c
+++ libc_stdio.c
@@ -1,3 +1,21 @@
+/*
+ * SHV - Small HyperVisor for testing nested virtualization in hypervisors
+ * Copyright (C) 2023  Eric Li
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /*-
  * Copyright (c) 1986, 1988, 1991, 1993
  *	The Regents of the University of California.  All rights reserved.
